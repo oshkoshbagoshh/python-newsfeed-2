ca0a3a2fc7f844280813a836a461caa48c484789
AJ <amirjavadi25@gmail.com>
2023-11-29 17:52:35 -0600

    updated comments.html and models

--- app/models/Vote.py
+++ app/models/Vote.py
@@ -1,19 +1,8 @@
-# @Author: AJ Javadi
-# @Email: amirjavadi25@gmail.com
-# @Date: 2023-11-28 11:41:08
-# @Last Modified by:   undefined
-# @Last Modified time: 2023-11-28 11:41:08
-# @Description: file:///Users/aj/python-newsfeed-2/app/models/Vote.py
-# Path: app/models/Vote.py
-
-# imports
 from app.db import Base
 from sqlalchemy import Column, Integer, ForeignKey
 
-# create a vote class
 class Vote(Base):
     __tablename__ = 'votes'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'))
     post_id = Column(Integer, ForeignKey('posts.id'))
-    
--- app/routes/api.py
+++ app/routes/api.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify, session
 from app.models import User, Post, Comment, Vote
 from app.db import get_db
-# from app.utils.auth import login_required
+from app.utils.auth import login_required
 import sys
 import traceback
 from sqlalchemy.orm.exc import NoResultFound
@@ -71,112 +71,112 @@ def login():
         return jsonify(message='An error occurred'), 500    
 
 
-# # comment route
-# @bp.route('/comments', methods=['POST'])
-# @login_required
-# def comment():
-#     data = request.get_json()
-#     db = get_db()
+# comment route
+@bp.route('/comments', methods=['POST'])
+@login_required
+def comment():
+    data = request.get_json()
+    db = get_db()
 
-#     try:
-#         # create a new comment
-#         newComment = Comment(
-#             comment_text = data['comment_text'],
-#             post_id = data['post_id'],
-#             user_id = session.get('user_id')
-#         )
-#         db.add(newComment)
-#         db.commit()
-#     except:
-#         print(sys.exc_info()[0])
-#         db.rollback()
-#         return jsonify(message = 'Comment failed'), 500
-#     return jsonify(id = newComment.id)
-
-
-# upvote route
-# @bp.route('/posts/upvote', methods=['PUT'])
-# @login_required
-# def upvote():
-#     data = request.get_json()
-#     db = get_db()
-
-#     try:
-#         # create a new vote with incoming id and session id
-#         newVote = Vote(
-#             post_id = data['post_id'],
-#             user_id = session.get('user_id')
-#         )
-
-#         db.add(newVote)
-#         db.commit()
-
-#     except:
-#         print(sys.exc_info()[0])
-
-#         db.rollback()
-#         return jsonify(message = 'Upvote failed'), 500
-#     return '', 204
-
-# # create a post
-# @bp.route('/posts', methods=['POST'])
-# @login_required
-# def create():
-#     data = request.get_json()
-#     db = get_db()
-
-#     try:
-#         # create a new post
-#         newPost = Post(
-#             title = data['title'],
-#             post_url = data['post_url'],
-#             user_id = session.get('user_id')
-#         )
-
-#         db.add(newPost)
-#         db.commit()
-
-#     except:
-#         print(sys.exc_info()[0])
-
-#         db.rollback()
-#         return jsonify(message = 'Post failed'), 500
-#     return jsonify(id = newPost.id)
+    try:
+        # create a new comment
+        newComment = Comment(
+            comment_text = data['comment_text'],
+            post_id = data['post_id'],
+            user_id = session.get('user_id')
+        )
+        db.add(newComment)
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+        db.rollback()
+        return jsonify(message = 'Comment failed'), 500
+    return jsonify(id = newComment.id)
 
 
-# # updating a single post
-# @bp.route('/posts/<id>', methods=['PUT'])
-# @login_required
-# def update(id):
-#     data = request.get_json()
-#     db = get_db()
+upvote route
+@bp.route('/posts/upvote', methods=['PUT'])
+@login_required
+def upvote():
+    data = request.get_json()
+    db = get_db()
 
-#     try:
-#         # retrieve post and update title property
-#         post = db.query(Post).filter(Post.id == id).one()
-#         post.title = data['title']
-#         db.commit()
-#     except:
-#         print(sys.exc_info()[0])
+    try:
+        # create a new vote with incoming id and session id
+        newVote = Vote(
+            post_id = data['post_id'],
+            user_id = session.get('user_id')
+        )
 
-#         db.rollback()
-#         return jsonify(message = 'Post not found'), 404
-#     return ''; 204
+        db.add(newVote)
+        db.commit()
+
+    except:
+        print(sys.exc_info()[0])
+
+        db.rollback()
+        return jsonify(message = 'Upvote failed'), 500
+    return '', 204
+
+# create a post
+@bp.route('/posts', methods=['POST'])
+@login_required
+def create():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+        # create a new post
+        newPost = Post(
+            title = data['title'],
+            post_url = data['post_url'],
+            user_id = session.get('user_id')
+        )
+
+        db.add(newPost)
+        db.commit()
+
+    except:
+        print(sys.exc_info()[0])
+
+        db.rollback()
+        return jsonify(message = 'Post failed'), 500
+    return jsonify(id = newPost.id)
 
 
-# # delete a post
-# @bp.route('/posts/<id>', methods=['DELETE'])
-# @login_required
-# def delete(id):
-#     db = get_db()
+# updating a single post
+@bp.route('/posts/<id>', methods=['PUT'])
+@login_required
+def update(id):
+    data = request.get_json()
+    db = get_db()
 
-#     try:
-#         # delete post from db
-#         db.delete(db.query(Post).filter(Post.id == id).one())
-#         db.commit()
-#     except:
-#         print(sys.exc_info()[0])
+    try:
+        # retrieve post and update title property
+        post = db.query(Post).filter(Post.id == id).one()
+        post.title = data['title']
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
 
-#         db.rollback()
-#         return jsonify(message = 'Post not found'), 404
-#     return '', 204
+        db.rollback()
+        return jsonify(message = 'Post not found'), 404
+    return ''; 204
+
+
+# delete a post
+@bp.route('/posts/<id>', methods=['DELETE'])
+@login_required
+def delete(id):
+    db = get_db()
+
+    try:
+        # delete post from db
+        db.delete(db.query(Post).filter(Post.id == id).one())
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+
+        db.rollback()
+        return jsonify(message = 'Post not found'), 404
+    return '', 204
